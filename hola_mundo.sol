128dae3dacb8a34c836a5acce1ddbc45326863fb
danielmontesalejandres <danielmontesalejandres@gmail.com>
2024-10-10 19:11:58 +0200

    First contract "hola mundo"

---
+++ hola_mundo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24; // defino la versión del compilador
+
+// empieza la lógica del contrato
+contract hola_mundo {
+    string public saludo = "Hola desde EducaETH 3.0"; // declaro el contenido de la variable saludo
+}
+
